4e40bb15d5913d49d2df53bf4f4138b62c900f8a
yiyern <yiyern@yahoo.com>
2018-08-09 16:29:53 +0800

    master

--- model/Auth.js
+++
@@ -1,9 +0,0 @@
-
-const Auth = function (data) {
-  this._id = data._id || '';
-  this.identity_type = data.identity_type || '';
-  this.identifier = data.identifier || '';
-  this.credential = data.credential || '';
-};
-
-module.exports = Auth
--- router/index.js
+++
@@ -1,7 +0,0 @@
-const mock = {}
-
-require('fs').readdirSync(require('path').join(`${__dirname}`)).filter(p => `${p}` !== 'index.js').forEach((file) => {
-  Object.assign(mock, require(`./${file}`))
-})
-
-module.exports = mock
