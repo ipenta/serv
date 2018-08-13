b903ee3313b5ba56a21034f4d4132c6f448981a7
yiyern <yiyern@yahoo.com>
2018-08-13 17:37:13 +0800

    project

--- server/project/project.router.js
+++ server/project/project.router.js
@@ -1,8 +1,22 @@
 const router = require('express').Router();
 
-router.route('/main')
+router.route('/')
   .get(function (res,resp) {
-    resp.json({"name":"project"});
+    resp.json({"name":"list project"});
   });
+  .post(function (res,resp) {
+    resp.json({"name":"create project"})
+  })
+
+router.route('/:id')
+  .get(function (res,resp) {
+    resp.json({"name":"show project"});
+  });
+  .patch(function (res,resp) {
+    resp.json({"name":"update project"})
+  })
+  .delete(function (res,resp) {
+    resp.json({"name":"delete project"})
+  })
 
 module.exports = router;
--- support/user/user.controller.js
+++
@@ -1,7 +0,0 @@
-
-const findOneById = function (req, resp, next) {
-
-  resp.json({name:'findOneById'})
-}
-
-module.exports = {findOneById};
