839e6e6be47d2c112275a156c8edcf9d5386dcb6
yiyern <yiyern@yahoo.com>
2018-08-15 18:26:27 +0800

    checkQuery

--- server/principal/controller.js
+++ server/principal/controller.js
@@ -16,37 +16,31 @@ const findById = function (req, resp, next) {
     }).catch(err => next(new APIError(err)))
 }
 
-const create = function (req, resp, next) {
+const checkQuery = function (req,resp,next) {
   const query = req.body
   const queryItem = {phonenum:query.phonenum}
-  model.findOne({ $or: [query,queryItem]})
+  return model.findOne({ $or: [query,queryItem]})
     .then(result => {
       if (result) {
         const statusText = (result.name === query.name) ? '委托信息未作修改':'委托电话已被注册'
         const err = new APIError(statusText,httpStatus.CREATED, true);
         return next(err);
       }
-    }).then(()=>{
-      model.create(query).then(result => resp.json(result))
+    })
+}
+
+const create = function (req, resp, next) {
+  checkQuery(req, resp, next).then(()=>{
+      model.create(req.body).then(result => resp.json(result))
         .catch(err => new APIError(err))
     }).catch(err => next(new APIError(err)))
 }
 
 const patch = function (req, resp, next) {
-  const query = req.body
-  const queryItem = {phonenum:query.phonenum}
-  model.find({ $or: [query,queryItem]})
-    .then(result => {
-      if (result) {
-        consol.log(result.name,query.name)
-        const statusText = (result.name === query.name) ? '委托信息未作修改':'委托电话已被注册'
-        const err = new APIError(statusText,httpStatus.CREATED, true);
-        return next(err);
-      }else {
-        model.update({ _id: req.params.id }, {$set:req.body} )
-          .then(result => resp.json(result))
-      }
-    }).catch(err => next(new APIError(err)))
+  checkQuery(req, resp, next).then(()=>{
+    model.update({ _id: req.params.id }, {$set:req.body} )
+      .then(result => resp.json(result))
+  }).catch(err => next(new APIError(err)))
 }
 
 const remove = function (req, resp) {
